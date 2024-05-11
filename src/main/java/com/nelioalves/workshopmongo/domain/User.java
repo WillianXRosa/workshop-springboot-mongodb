b79fda93c5df293c0b193d7be85e8111994506a2
WillianXRosa <wxrosa@hotmail.com>
2024-05-11 20:17:23 -0300

    11/05/2024 - Criacao das Classes EntityUser e ResourceUser, Classes funcionando

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -1,12 +1,19 @@
 package com.nelioalves.workshopmongo.domain;
 
-public class User {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {	
+	
+	private static final long serialVersionUID = 1L;
 	
 	private String id;
 	private String nome;
 	private String email;
 	
-	
+	public User (){
+		
+	}
 	
 	public User(String id, String nome, String email) {
 		super();
@@ -34,6 +41,25 @@ public class User {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
