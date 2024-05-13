dc84c5095f97ce574c6756b9fb6b4a5d1907f229
WillianXRosa <wxrosa@hotmail.com>
2024-05-13 06:22:28 -0300

    13/05/2024 - Criacao da Classe UserDTO para retorno de Usuarios Cadastrados.

--- src/main/java/com/nelioalves/workshopmongo/resource/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resource/UserResource.java
@@ -1,8 +1,11 @@
 package com.nelioalves.workshopmongo.resource;
 
 import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.services.UserService;
 
 @RestController
@@ -21,9 +25,11 @@ public class UserResource {
 	private UserService userService;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
-				
-		List<User> list = userService.findAll();		
-		return ResponseEntity.ok().body(list);
+	public ResponseEntity<List<UserDTO>> findAll(){
+		
+		List<User> list = userService.findAll();	
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+		
 	}
 }
